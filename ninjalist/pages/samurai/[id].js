dab3e907fc57a401fe0f1d790e02081e16878d88
Akhil Venkatesh Pillai <akhiltechwiz@gmail.com>
2023-01-09 00:08:20 +0530

    feat
    introduced dynamic routing

--- ninjalist/pages/samurai/[id].js
+++ ninjalist/pages/samurai/[id].js
@@ -2,9 +2,43 @@ import styles from "../../styles/Home.module.css"
 import Head from "next/head"
 import Link from "next/link"
 
-const Details = () => {
+
+export const getStaticPaths = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+
+    const paths = data.map(samurai=>{
+        return {
+            params:{id: samurai.id.toString()}
+        }
+    })
+  
+    return {
+      paths,
+      fallback: false
+    }
+  }
+ 
+  
+  export const getStaticProps = async (context) => {
+      const id =  context.params.id;
+    const res = await fetch("https://jsonplaceholder.typicode.com/users/"+id);
+    const data = await res.json();
+  
+    return {
+      props : {samurai : data}
+    }
+  }
+   
+
+const Details = ({samurai}) => {
+
+   
     return ( <div className={styles.detailContent}>
-        <h1>Details</h1>
+        <h1>{samurai.name}</h1>
+        <p>{samurai.email}</p>
+        <p>{samurai.address.city}</p>
+        <p>{samurai.phone}</p>
     </div> );
 }
  
